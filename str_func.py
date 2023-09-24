cf9ed0b2b8289edee297db98bd905a96eb89c389
Grebneva Marina <grmarcw@gmail.com>
2023-09-24 15:28:01 +0500

    second func(up_first) and docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def up(string):
+    '''все буквы переводит в верх.регистр'''
     result = string.upper()
+    return result
+
+def up_first(string):
+    '''первые буквы слова переводит в верх.регистр'''
+    result = string.title
     return result
